dfe0070b5487b2dd18468ba973b21001da621107
idavid80 <dominguezbueno.david@gmail.com>
2022-11-04 21:33:00 +0100

    css con material

--- pomodoro-clock/src/Pomodoro-Clock/Pomodoro.js
+++ pomodoro-clock/src/Pomodoro-Clock/Pomodoro.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import "./Pomodoro.css";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+
+const color = red[500];
 
 const Pomodoro = (props) => {
   let trabajo = 3;
@@ -51,27 +59,31 @@ const Pomodoro = (props) => {
   const display = (digito) => digito.toString().padStart(2, "0");
 
   return (
-    <div className="temporizador">
-      <h3>{metodo === true ? "Trabajando" : "Descansando"}</h3>
-      <p>
-        {display(minutos)}:{display(segundos)}
-      </p>
-      <div className="botones">
-        <button
-          onClick={reiniciarTiempo}
-          disabled={segundos === 0 && minutos === 0 ? false : true}
-        >
+    <Container maxWidth="sm"  justifyContent="center" alignItems="center">
+      <Stack direction="column" spacing={1}  justifyContent="center" alignItems="center">
+        <Typography color={color} variant="h6">
+          {metodo === true ? "Trabajando" : "Descansando"}
+        </Typography>
+        <Typography variant="h3" color={color}>
+          {display(minutos)}:{display(segundos)}
+        </Typography>
+        </Stack>
+        <Stack direction="row" spacing={1}  justifyContent="center">
+        <Button size="large" variant="outlined" onClick={reiniciarTiempo}>
           {metodo === undefined
             ? "Empezar"
             : segundos === 0 && minutos === 0
             ? "Continuar"
             : "Ciclo " + sesiones}
-        </button>
-        <button onClick={pausarTiempo}>
-          {pausa === true ? "Reanudar" : "Pausar"}
-        </button>
-      </div>
-    </div>
+        </Button>
+        <Button size="large" variant="outlined" onClick={pausarTiempo}>
+          <Tab
+            icon={<AlarmIcon />}
+            label={pausa === true ? "Reanudar" : "Pausar"}
+          />
+        </Button>
+      </Stack>
+    </Container>
   );
 };
 export default Pomodoro;
